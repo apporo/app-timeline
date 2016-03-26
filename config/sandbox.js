a0d10d7db12fff8ae145e435b671094176dfd92a
Pham Ngoc Hung <pnhung177@drupalex.net>
2016-03-26 08:02:16 +0700

    Load data from database and display to timeline page

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,2 +1,18 @@
 module.exports = {
+  bridges: {
+    timelineMongodbWrapper: {
+      mongodb: {
+        connection_options: {
+          host: '127.0.0.1',
+          port: '27017',
+          name: 'suviet_unknown'
+        }
+      }
+    }
+  },
+  plugins: {
+    appTimeline: {
+      contextPath: '/timeline'
+    }
+  }
 };
